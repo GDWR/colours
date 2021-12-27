af06e7a34e98a2ba3238432b14d388872f660bf6
GDWR <gregory.dwr@gmail.com>
2021-12-27 23:19:52 +0000

    Black formatting

--- run.py
+++ run.py
@@ -5,9 +5,7 @@ import uvicorn
 from PIL import Image, ImageFont, ImageColor
 from PIL.ImageDraw import ImageDraw
 
-colour_mapping = {
-    "fuschia": (255, 0, 255)
-}
+colour_mapping = {"fuschia": (255, 0, 255)}
 
 
 class Server:
@@ -15,10 +13,12 @@ class Server:
         path = scope["path"]
 
         if path == "/favicon.ico":
-            await send({
-                'type': 'http.response.start',
-                'status': 404,
-            })
+            await send(
+                {
+                    "type": "http.response.start",
+                    "status": 404,
+                }
+            )
             return
 
         colours = path[1:].replace("&", "-").split("-")
@@ -26,30 +26,38 @@ class Server:
         try:
             im = self.generate_image_from_hexes(*colours)
         except ValueError:
-            await send({
-                'type': 'http.response.start',
-                'status': 400,
-            })
+            await send(
+                {
+                    "type": "http.response.start",
+                    "status": 400,
+                }
+            )
             return
 
-        await send({
-            'type': 'http.response.start',
-            'status': 200,
-            'headers': [
-                [b'content-type', b'image/png'],
-            ]
-        })
+        await send(
+            {
+                "type": "http.response.start",
+                "status": 200,
+                "headers": [
+                    [b"content-type", b"image/png"],
+                ],
+            }
+        )
 
         with BytesIO() as buffer:
             im.save(buffer, format="PNG")
 
-            await send({
-                'type': 'http.response.body',
-                'body': buffer.getvalue(),
-            })
+            await send(
+                {
+                    "type": "http.response.body",
+                    "body": buffer.getvalue(),
+                }
+            )
 
     @staticmethod
-    def generate_image_from_hexes(*hex_codes: str, width: int = 1920, height: int = 1080) -> Image:
+    def generate_image_from_hexes(
+        *hex_codes: str, width: int = 1920, height: int = 1080
+    ) -> Image:
         """
         From a list of hex codes in the format #FFFFFF
         Create an image split evenly vertically depicting each colour.
@@ -60,7 +68,7 @@ class Server:
         :return: Image
         """
 
-        im = Image.new('RGBA', (width, height), (255, 0, 0, 0))
+        im = Image.new("RGBA", (width, height), (255, 0, 0, 0))
 
         fnt_size = round(40 / (len(hex_codes) / 5))
         fnt = ImageFont.truetype("LemonMilkMedium.otf", fnt_size)
@@ -86,8 +94,12 @@ class Server:
             draw.rectangle((x0, 0, x1, height), fill=colour)
 
             text_im_width = math.ceil(x1 - x0)
-            text_im = Image.new('RGBA', (text_im_width, math.floor(fnt_size * 1.7)), (0, 0, 0, 100))
-            ImageDraw(text_im).text(((text_im_width // 2) - (fnt.getlength(code) // 2), 0), code, font=fnt)
+            text_im = Image.new(
+                "RGBA", (text_im_width, math.floor(fnt_size * 1.7)), (0, 0, 0, 100)
+            )
+            ImageDraw(text_im).text(
+                ((text_im_width // 2) - (fnt.getlength(code) // 2), 0), code, font=fnt
+            )
 
             im.paste(text_im, box=(math.floor(x0), 0), mask=text_im)
 
